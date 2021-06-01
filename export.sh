61e75f6fdaebf10bdae47d42dcabf3af3caefa3f
ThongSong7 <jeff@jeffkoon.eu>
2021-06-01 08:56:46 +0200

    Update export.sh

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ## To execute on ESX
-## Usage: ./export_vm.sh %VMNAME%
+## Usage: ./export_vm.sh %VMNAME% (visible in inventory)
 vm=$1
 vmid=$(echo `vim-cmd vmsvc/getallvms | grep "$vm" | awk '{print $1}'`)
 state=$(vim-cmd vmsvc/power.getstate $vmid | awk '{ print $2 }' | tail -n +2)
@@ -10,8 +10,8 @@ repo=/location/on/esx/where/storing/ovf
 if [[ $state == on ]]
 then
   vim-cmd vmsvc/power.shutdown $vmid
-# Wait till the VM is fully off
-echo "Waiting for machine $vmid to poweroff..."
+## Wait till the VM is fully off
+echo "Waiting for $vm to poweroff..."
 until vim-cmd vmsvc/power.getstate $vmid | grep -i "off" > /dev/null 2<&1
 do
   sleep 5
