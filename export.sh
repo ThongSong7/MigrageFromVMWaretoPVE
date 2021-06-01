f581124cbceee714cf8b5fcb3a98df32b104352d
ThongSong7 <jeff@jeffkoon.eu>
2021-06-01 08:19:53 +0200

    Update export.sh
    Totally changed bash.... will now wait for poweroff of machine to start export to avoid errors.

--- export.sh
+++ export.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
+## To be ran on ESX
+## Usage: ./export_vm.sh %VMNAME%
 vm=$1
 vmid=$(echo `vim-cmd vmsvc/getallvms | grep "$vm" | awk '{print $1}'`)
 state=$(vim-cmd vmsvc/power.getstate $vmid | awk '{ print $2 }' | tail -n +2)
+ovftool=/location/on/esx/where/vmware-ovftool
+repo=/location/on/esx/where/storing/ovf
 
 if [[ $state == on ]]
 then
   vim-cmd vmsvc/power.shutdown $vmid
-  sleep 10  # Give the VM time to shutdown gracefully
-  /location/to/where/vmware-ovftool/ovftool --noSSLVerify -dm=thin --parallelThreads=6 vi://root:PASSWORDHERE@localhost/"$vm" /location/to/save/OVF/Folder
+# Wait till the VM is fully off
+echo "Waiting for machine $vmid to poweroff..."
+until vim-cmd vmsvc/power.getstate $vmid | grep -i "off" > /dev/null 2<&1
+do
+  sleep 1
+done
+  $ovftool/ovftool --noSSLVerify -dm=thin --parallelThreads=6 vi://root:PASSWORDHERE@localhost/"$vm" $repo || vim-cmd vmsvc/power.on $vmid
   vim-cmd vmsvc/power.on $vmid
 else
-  /location/to/where/vmware-ovftool/ovftool --noSSLVerify -dm=thin --parallelThreads=6 vi://root:PASSWORDHERE@localhost/"$vm" /location/to/save/OVF/Folder
+  $ovftool/ovftool --noSSLVerify -dm=thin --parallelThreads=6 vi://root:PASSWORDHERE@localhost/"$vm" $repo || vim-cmd vmsvc/power.on $vmid
   vim-cmd vmsvc/power.on $vmid
 fi
 exit 0
